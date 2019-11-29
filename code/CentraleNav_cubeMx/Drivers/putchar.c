7316817a64c1f61eab29bd98b137470858ea2d51
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2019-11-29 17:07:31 +0100

    Import code initial.

---
+++ code/CentraleNav_cubeMx/Drivers/putchar.c
@@ -0,0 +1,35 @@
+/*
+ * printf.c
+ *
+ *  Created on: 29 nov. 2019
+ *      Author: gom
+ */
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+#ifdef __GNUC__
+/* With GCC, small printf (option LD Linker->Libraries->Small printf
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+
+
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART2 and Loop until the end of transmission */
+  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+
+  return ch;
+}
