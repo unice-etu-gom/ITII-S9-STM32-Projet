e2e41ed31f7fbff15d64e4ce63a4aaf466fa266c
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2019-11-30 08:32:56 +0100

    Ajout gestion LEDs par défaut.

--- code/CentraleNav_cubeMx/user/init.c
+++ code/CentraleNav_cubeMx/user/init.c
@@ -2,11 +2,21 @@
 
 #include <stdio.h>
 
+#include "stm32412g_discovery.h"
+
 /* ########################################################################## */
 /* ########################################################################## */
 
 void	init(void)
 {
+	  BSP_LED_Init(LED_BLUE);
+	  BSP_LED_Init(LED_RED);
+	  BSP_LED_Init(LED_ORANGE);
+	  BSP_LED_Init(LED_GREEN);
+
+	  BSP_LED_On(LED_GREEN);
+
+
 	  printf("\n\nSTM32F412Discovery: Hello, World!\n");
 	  printf("Alive");
 	  fflush(stdout);
--- code/CentraleNav_cubeMx/user/loop.c
+++ code/CentraleNav_cubeMx/user/loop.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 
+#include "stm32412g_discovery.h"
 #include "stm32f4xx_hal.h"
 
 /* ########################################################################## */
@@ -11,6 +12,7 @@ void	loop(void)
 {
 	  printf(".");
 	  fflush(stdout);
+	  BSP_LED_Toggle(LED_GREEN);
 
 	  HAL_Delay(1000);
 }
