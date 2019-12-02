b6d9407d14cda6e4c11d97d52bf307e4251ea91d
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2019-12-02 15:16:39 +0100

    Implémentation de deux flèches "previous"/"next" sur vue "Main".

---
+++ code/CentraleNav_cubeMx/user/display/common/ui_button.c
@@ -0,0 +1,135 @@
+/* Corresponding header inclusion */
+#include "ui_button.h"
+
+/* System includes */
+#include <assert.h>
+
+/* Libraries includes */
+
+/* Project includes */
+
+
+/* ########################################################################## */
+/* ########################################################################## */
+
+void    ui_button_draw(
+                TsUiButton* pButton )
+{
+    /* Draw the background */
+    BSP_LCD_SetTextColor(pButton->colorBG);
+    BSP_LCD_FillRect(
+                pButton->origX,
+                pButton->origY,
+                pButton->sizeX,
+                pButton->sizeY );
+
+
+    /* Draw the image */
+    if( pButton->imagePtr != 0 )
+    {
+        BSP_LCD_DrawRGBImage(
+                    pButton->origX,
+                    pButton->origY,
+                    pButton->imagePtr->width,
+                    pButton->imagePtr->height,
+                    pButton->imagePtr->pixel_data );
+    }
+}
+
+/* ########################################################################## */
+/* ########################################################################## */
+
+void    ui_button_init(TsUiButton *pButtonPtr)
+{
+    assert(     pButtonPtr != 0
+            &&  "pButton must be a valid struct!" );
+
+
+    pButtonPtr->colorBG = LCD_COLOR_GRAY;
+    pButtonPtr->colorFG = LCD_COLOR_BLACK;
+
+    pButtonPtr->origX   = 0U;
+    pButtonPtr->origY   = 0U;
+
+    pButtonPtr->sizeX   = 60U;
+    pButtonPtr->sizeY   = 60U;
+
+    pButtonPtr->imagePtr    = 0U;
+}
+
+/* ########################################################################## */
+/* ########################################################################## */
+
+void    ui_button_setColorBackground(
+                TsUiButton* pButtonPtr,
+                uint16_t    pColor )
+{
+    pButtonPtr->colorBG    = pColor;
+}
+
+/* ########################################################################## */
+/* ########################################################################## */
+
+void    ui_button_setColorForeground(
+                TsUiButton* pButton,
+                uint16_t    pColor )
+{
+    pButton->colorFG    = pColor;
+}
+
+/* ########################################################################## */
+/* ########################################################################## */
+
+void    ui_button_setImage(
+                TsUiButton*     pButton,
+                TsGimpImage*    pImagePtr )
+{
+    pButton->imagePtr   = pImagePtr;
+}
+
+/* ########################################################################## */
+/* ########################################################################## */
+
+void    ui_button_setOrig(TsUiButton *pButtonPtr,
+                uint16_t    pOrigX,
+                uint16_t    pOrigY )
+{
+    pButtonPtr->origX  = pOrigX;
+    pButtonPtr->origY  = pOrigY;
+}
+
+/* ########################################################################## */
+/* ########################################################################## */
+
+uint8_t ui_button_touchIsIn(
+                TsUiButton*         pButton,
+                TS_StateTypeDef*    pTSStatePtr )
+{
+    uint8_t retval  = 0;
+
+
+    if( pTSStatePtr->touchDetected == 0 )
+    {
+        retval  = 0;
+    }
+    else if(    pTSStatePtr->touchX[0] < pButton->origX
+            ||  pTSStatePtr->touchX[0] > pButton->origX + pButton->sizeX )
+    {
+        retval  = 0;
+    }
+    else if(    pTSStatePtr->touchY[0] < pButton->origY
+            ||  pTSStatePtr->touchY[0] > pButton->origY + pButton->sizeY )
+    {
+        retval  = 0;
+    }
+    else
+    {
+        retval  = 1;
+    }
+
+
+    return retval;
+}
+
+/* ########################################################################## */
+/* ########################################################################## */
---
+++ code/CentraleNav_cubeMx/user/display/common/ui_button.h
@@ -0,0 +1,71 @@
+#ifndef UI_BUTTON_H
+#define UI_BUTTON_H
+
+/* Inherited classes */
+
+/* System includes */
+#include <stdint.h>
+
+/* Libraries includes */
+#include "stm32412g_discovery_lcd.h"
+#include "stm32412g_discovery_ts.h"
+
+/* Project includes */
+#include "../images/TsGimpImage.h"
+
+
+/* ########################################################################## */
+/* ########################################################################## */
+
+typedef struct  _SUiButton
+{
+    uint16_t    origX;
+    uint16_t    origY;
+    uint16_t    sizeX;
+    uint16_t    sizeY;
+
+    uint16_t    colorBG;
+    uint16_t    colorFG;
+
+    TsGimpImage*    imagePtr;
+
+}   TsUiButton;
+
+/* ########################################################################## */
+/* ########################################################################## */
+
+void    ui_button_init(
+                TsUiButton* pButton );
+
+
+void    ui_button_draw(
+                TsUiButton* pButton );
+
+
+void    ui_button_setOrig(
+                TsUiButton* pButtonPtr,
+                uint16_t    pOrigX,
+                uint16_t    pOrigY );
+
+void    ui_button_setColorBackground(
+                TsUiButton* pButtonPtr,
+                uint16_t    pColor );
+
+void    ui_button_setColorForeground(
+                TsUiButton* pButton,
+                uint16_t    pColor );
+
+void    ui_button_setImage(
+                TsUiButton*     pButton,
+                TsGimpImage*    pImagePtr );
+
+
+
+uint8_t ui_button_touchIsIn(
+                TsUiButton*         pButton,
+                TS_StateTypeDef*    pTSStatePtr );
+
+/* ########################################################################## */
+/* ########################################################################## */
+
+#endif  /*< UI_BUTTON_H */
