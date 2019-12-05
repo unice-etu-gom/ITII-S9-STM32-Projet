1fde280239eb8c18b8948563f1961af1dcd288f7
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2019-12-05 20:13:58 +0100

    Fix warnings dus a une difference de type dans les structures TsGimpImage.

--- code/CentraleNav_cubeMx/user/display/images/TsGimpImage.h
+++ code/CentraleNav_cubeMx/user/display/images/TsGimpImage.h
@@ -1,6 +1,8 @@
 #ifndef TSGIMPIMAGE_H
 #define TSGIMPIMAGE_H
 
+#include <stdint.h>
+
 /* ########################################################################## */
 /* ########################################################################## */
 
@@ -28,7 +30,7 @@ typedef struct {
     unsigned int    width;
     unsigned int    height;
     unsigned int    bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */
-    unsigned char   pixel_data[];
+    uint8_t         pixel_data[];
 } TsGimpImage;
 
 /* ########################################################################## */
