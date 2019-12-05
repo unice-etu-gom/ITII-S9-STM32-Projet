3c02eedc0a63561e915d107859383da171c60d60
UNICE_ETU/GOM <mathias.go@etu.unice.fr>
2019-12-05 20:30:03 +0100

    tmp: Modification périodique des valeurs du magnétomètre.

--- code/CentraleNav_cubeMx/user/callbacks/TIM_PeriodElapsedCallback.c
+++ code/CentraleNav_cubeMx/user/callbacks/TIM_PeriodElapsedCallback.c
@@ -11,14 +11,20 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *pTimerHandle)
     if( pTimerHandle == &htim13 )
     {
         g_flag_delayedActions_10ms  = 1;
+#warning Magnetometer value is modified here for tests only !
+        g_magnetometerValues.X++;
     }
     else if( pTimerHandle == &htim12 )
     {
         g_flag_delayedActions_100ms = 1;
+#warning Magnetometer value is modified here for tests only !
+        g_magnetometerValues.Y++;
     }
     else if( pTimerHandle == &htim11 )
     {
         g_flag_delayedActions_1000ms    = 1;
+#warning Magnetometer value is modified here for tests only !
+        g_magnetometerValues.Z++;
     }
     else
     {
